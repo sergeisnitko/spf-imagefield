7a1bde86ab70d4c08083b92efc12d32eaf6d7799
Sergei Snitko <sergei.snitko@cibpoint.com>
2016-09-09 23:26:50 +0300

    main functions

---
+++ spf-imagefield/Program.cs
@@ -0,0 +1,26 @@
+﻿using SP.Cmd.Deploy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spf_imagefield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SharePoint.CmdExecute(args, "SPF Image Field",
+                options =>
+                {
+                    Model.Deploy(options);
+                },
+                null,
+                null
+            );
+
+            var t = "";
+        }
+    }
+}
